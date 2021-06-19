a0f03a722febc94bd9153cc0b28bae597ea86438
VanderSant <vanderson.santos@usp.br>
2021-06-19 17:17:30 -0300

    🚚 givens_rotations -> QR_fatoration

--- src/ep1/QR_fatoration.py
+++ src/ep1/QR_fatoration.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-## Function to get the Qk matrix for givens rotation
-def Qk(k,A):
+## Function to get the givens_rotation_Qk matrix for givens rotation
+def givens_rotation_Qk(k,A):
     Q = np.eye(len(A))
     alfa_k = A[k,k]
     beta_k = A[k+1,k]
@@ -15,16 +15,16 @@ def Qk(k,A):
     pass
 
 ## Function which returns a matrix after a givens rotation
-def givens_rotations(A):
+def QR_fatoration(A):
     R = A
     matrix_i_size = len(A)
+    Q =np.eye(matrix_i_size)
     Q_iteration_size = matrix_i_size - 1
     for i in range (0, Q_iteration_size,1):
-        print("matriz R:", R,"\n")
-        print("matriz Q:", Qk(i,R),"\n")
-        R = Qk(i,R)@R
+        Q = Q@np.transpose(givens_rotation_Qk(i,R))
+        R = givens_rotation_Qk(i,R)@R
         R = np.matrix.round(R,5)
-    return R
+    return [R,Q]
     pass
 
 #exemple 1:
@@ -32,5 +32,7 @@ A = np.array([[2,1,0,0],
               [1,2,1,0],
               [0,1,2,1],
               [0,0,1,2]])
-
-print(givens_rotations(A))
+R = QR_fatoration(A)[0]
+Q = QR_fatoration(A)[1]
+print("R = ", R)
+print("Q = ", Q)
