051841c44dfa2fc2e33bd8b2e5a55e536b53c651
VanderSant <vanderson.santos@usp.br>
2021-06-23 04:34:55 -0300

    🔨  add grafics plots

--- src/ep1/activity_a.py
+++ src/ep1/activity_a.py
@@ -6,40 +6,55 @@ import matplotlib.dates as mdates
 from QR_fatoration import QR_fatoration
 from QR_algorithm import QR_algorithm
 
+def make_tridiagonal_matrix(n,alfa,beta):
+    A = alfa*np.eye(n)
+    for i in range (0,n-1):
+        A[i,i+1] = beta
+        A[i+1,i] = beta
+    return A
+    pass
 
-
-
-
-
-
+def test_matrix_values(A,V,lamb):
+    print("V = ", V)
+    print("lamb = ",lamb)
+    new_A = V@lamb@np.transpose(V)
+    new_A = np.matrix.round(new_A,3)
+    print("\n")
+    print("A = \n", A)
+    print("V Λ V_transposto = \n", new_A)
+    pass
 
 
 if __name__ == "__main__":
     erro = 1e-6
-    for i in range (2,6):
-        #n = int(input('insert matrix size n (n x n): '))
-        n = 2**i
-        A_matrix = 2*np.eye(n)
-        for i in range (0,n-1):
-            A_matrix[i,i+1] = -1
-            A_matrix[i+1,i] = -1
+    alfa = 2
+    beta = -1
 
-        V,lamb,k = QR_algorithm(A_matrix,erro = erro,spectral_shift = True )
-        print("V = ", V)
-        print("lamb = ",lamb)
+    k_with_spectral_shift_list = np.array([])
+    k_without_spectral_shift_list = np.array([])
+    n_list = np.array([])
+    for i in range (2,6):
+        n = 2**i
+        n_list = np.append(n_list,n)
+        A_matrix = make_tridiagonal_matrix(n,alfa,beta)
+
+        V,lamb,k = QR_algorithm(A_matrix,erro = erro,spectral_shift = True)
+        k_with_spectral_shift_list = np.append(k_with_spectral_shift_list,k)
         print("k with spectral shift = ", k)
-        new_A = V@lamb@np.transpose(V)
-        new_A = np.matrix.round(new_A,3)
-        print("\n")
-        print("A = \n", A_matrix)
-        print("V Λ V_transposto = \n", new_A)
+        #test_matrix_values(A_matrix,V,lamb)
 
         V,lamb,k = QR_algorithm(A_matrix,erro = erro,spectral_shift = False)
-        print("V = ", V)
-        print("lamb = ",lamb)
-        print("k without spectral shift= ", k)
-        new_A = V@lamb@np.transpose(V)
-        new_A = np.matrix.round(new_A,3)
-        print("\n")
-        print("A = \n", A_matrix)
-        print("V Λ V_transposto = \n", new_A)
+        k_without_spectral_shift_list = np.append(k_without_spectral_shift_list,k)
+        print("k without spectral shift = ", k)
+        #test_matrix_values(A_matrix,V,lamb)
+
+    plt.xlim(min(n_list), max(n_list))
+    plt.ylim(0, 2000)
+
+    plt.plot(n_list,k_with_spectral_shift_list,label='k_with_spectral_shift')
+    plt.plot(n_list,k_without_spectral_shift_list,label='k_without_spectral_shift')
+    plt.ylabel('number of iterations')
+    plt.xlabel('matrix (nxn)')
+
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=2, frameon=False)
+    plt.show()
