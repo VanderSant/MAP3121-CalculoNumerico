38222320ebf843d4221fe9e4d32a5c1fcbd0a00e
VanderSant <vanderson.santos@usp.br>
2021-07-17 14:09:41 -0300

    🏗️  build a prototype architecture
    Co-authored-by: AntonioSeixas1 <AntonioSeixas1@users.noreply.github.com>

---
+++ src/ep2/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+EP_HEADER = '''
+===============================================================================================
+                            MAP3121 - Métodos Numéricos e Aplicações
+-----------------------------------------------------------------------------------------------
+2° Exercício Programa - O Algoritmo QR
+
+11261110 - Antonio Lago Araújo Seixas                                                 turma: 1
+11259715 - Vanderson da Silva dos Santos                                              turma: 2
+===============================================================================================
+'''
+
+INFO = '''
+TAREFAS:
+(a) Tarefa A: Matriz com diagonal e subdiagonal constantes
+(b) Tarefa B: Sistema Massa Mola com 5 massas
+(c) Tarefa C: Sistema Massa Mola com 10 massas
+'''
