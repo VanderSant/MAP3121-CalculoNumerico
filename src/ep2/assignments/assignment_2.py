9a4b9a3be15aea47f2a8db8eda1dcea57b8b7b02
VanderSant <vanderson.santos@usp.br>
2021-07-18 13:55:47 -0300

    🏗️  add assignments prototype

---
+++ src/ep2/assignments/assignment_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+def assignment_2():
+    pass
+
+if __name__ == "__main__":
+    try:
+        assignment_2()
+
+    except KeyboardInterrupt:
+        print("\n Better luck next time")
