84be592a38104ce9c21e9f505b204720ab9b5d39
VanderSant <vanderson.santos@usp.br>
2021-07-20 01:30:21 -0300

    🔨  update assignment_2

--- src/ep2/assignments/assignment_2.py
+++ src/ep2/assignments/assignment_2.py
@@ -24,6 +24,9 @@ class Beam:
         self.K = 0
         self.make_k_matrix()
 
+    def __str__(self):
+        return 'no1 ={no_1}, no2 ={no_2}, ang ={ang}, lenght ={lenght}\n'.format(no_1=self.no1,no_2=self.no2,ang=self.ang,lenght=self.lenght)
+
     def make_k_matrix(self):
         x = (self.A*self.E)/self.lenght
         C = np.cos(self.ang)
@@ -51,14 +54,18 @@ def read_input_c():
                 (file_information[i])[j] = float((file_information[i])[j])
     return file_information
 
+def test_class_beam():
+    file_info = read_input_c()
+    print(file_info,"\n")
+    beams = np.array([])
+    for i in range(0,NUM_TRE):
+        info = file_info[i]
+        beams = np.append(beams,Beam(info[0],info[1],info[2],info[3]))
+    print(beams[0],"\n",beams[0].get_K())
+
 def make_total_K_matrix(V):
     K = np.zeros(24)
     for i in range (0,len(V)):
-        # Jeito completamente corno kkkkkkkk mas não encontrei nenhum padrão, da uma olhadinha no enunciado
-        # Ta mais pra pseudocódigo pq n sei exatamente como acessa os valores haha
-        # Vou comer aqui, aí ja volto
-
-        #KKKKKKKKKKKKKKKKk vou olhar
         aux_k = V[i].get_K()
         K[2*(V[i].no1)-1,2*(V[i].no1)-1] += aux_k[1,1]
         K[2*(V[i].no1)-1,2*(V[i].no1)] += aux_k[1,2]
@@ -79,12 +86,11 @@ def make_total_K_matrix(V):
 
 def assignment_2():
     file_info = read_input_c()
-    print(file_info)
+    print(file_info,"\n")
     beams = np.array([])
     for i in range(0,NUM_TRE):
         info = file_info[i]
         beams = np.append(beams,Beam(info[0],info[1],info[2],info[3]))
-    print(beams[0].get_K())
 
 if __name__ == "__main__":
     try:
